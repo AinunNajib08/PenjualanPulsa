a2cee868eadc6c4ac0764ce7f657dbb6e9249bf6
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-05-04 17:18:34 +0700

    Create belipulsa.java

---
+++ ForAndroid/app/src/main/java/android/com/penjualanpulsa/belipulsa.java
@@ -0,0 +1,13 @@
+package android.com.penjualanpulsa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class belipulsa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belipulsa);
+    }
+}
