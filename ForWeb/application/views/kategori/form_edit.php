396bb401fc7c4cfa4a3613c64d89d90c57209242
waragafila <ajicheater@gmail.com>
2019-05-06 21:47:05 +0700

    perbaikan kategori

--- ForWeb/application/views/kategori/form_edit.php
+++ ForWeb/application/views/kategori/form_edit.php
@@ -34,7 +34,7 @@ $this->load->view('template/sidebaradmin');
 						<td><div class="col-sm-4""><input type="text" name="kategori" placeholder="kategori" class="form-control" value="<?php echo $record['nama_kategori']?>"></div>
 						</td></tr>
 						 <tr><td colspan="2"><button type="submit" class="btn btn-primary btn-sm" name="submit">Simpan</button> 
-						 <?php echo anchor('kategori','Kembali',array('class'=>'btn btn-primary btn-sm'))?></td></tr>
+						 <?php echo anchor('index.php/kategori','Kembali',array('class'=>'btn btn-primary btn-sm'))?></td></tr>
 				</table>
 						</form>
 <!-- Akhir Coding  Data -->
--- ForWeb/application/views/kategori/form_input.php
+++ ForWeb/application/views/kategori/form_input.php
@@ -33,7 +33,7 @@ $this->load->view('template/sidebaradmin');
 					        <td><div class="col-sm-4""><input type="text" name="kategori" class="form-control" placeholder="kategori"></div>
 					       </td></tr>
 					    <tr><td colspan="2"><button type="submit" class="btn btn-primary btn-sm" name="submit">Simpan</button>
-					        <?php echo anchor('kategori','Kembali',array('class'=>'btn btn-primary btn-sm'))?>
+					        <?php echo anchor('index.php/kategori','Kembali',array('class'=>'btn btn-primary btn-sm'))?>
 					        </td></tr>
 					</table>
 					</form>
