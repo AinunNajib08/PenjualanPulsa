20e164ddc70957af4ed2bbcf1b66cf229dca4b77
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-05-26 17:07:55 +0700

    data

---
+++ ForAndroid/ClientAndroid/app/src/main/java/com/najib/clientandroid/Pembelian.java
@@ -0,0 +1,13 @@
+package com.najib.clientandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pembelian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembelian);
+    }
+}
